030c118adb6df31a6089fd8c50ea9849a0e7ad17
Kekilla0 <64717385+Kekilla0@users.noreply.github.com>
2020-08-28 20:40:54 -0400

    Update dialog.js

--- scripts/dialog.js
+++ scripts/dialog.js
@@ -67,7 +67,7 @@ export function newDialog({content = ``, title = ``, whisper = [], skipDialog = 
       return `<option value="${id}"${whispered}>${name}</option>`;
     });
 
-    let selectedOption = whisper.length > 0 ? whisperOptions : selectOptions;
+    //let selectedOption = whisper.length > 0 ? whisperOptions : selectOptions;
 
     //check if the client already wants a chatWhisper
     let checked = chatWhisper ? `checked` : ``;
