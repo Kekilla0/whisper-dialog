af2177f49ea25121b98a32fd9dd30e12681b4ae0
Shawn Dibble <shawndibble@gmail.com>
2020-10-21 20:54:43 -0500

    Update sidebar for 0.7.5
    Update the sidebar icon for 0.7.5

--- scripts/sidebar.js
+++ scripts/sidebar.js
@@ -2,11 +2,10 @@ import * as dialog from './dialog.js';
 
 export function renderWhisperIcon() {
     const lang_html = $(`
-    <a class="button whisper-dialog" title="whisper selected players">
+    <a class="chat-control-icon whisper-dialog" title="whisper selected players" style="margin-right: 7px">
             <i class="fas fa-user-secret"></i>
     </a>
     `);
-    jQuery(".roll-type-select label").before(lang_html);
+    jQuery("#chat-controls label").before(lang_html);
     jQuery("a.whisper-dialog").click(() => dialog.newDialog());
-    jQuery("select[name=rollMode]").css('width', '170px');
 }
